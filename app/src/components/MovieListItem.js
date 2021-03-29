ef99ccc71c235efd752efa0da8f82ea635504cd9
Paul Berry <paulberry96@gmail.com>
2021-03-29 10:03:19 +1000

    Made genre and year clickable

--- app/src/components/MovieListItem.js
+++ app/src/components/MovieListItem.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useStore } from "../stores/RootStore";
 import './MovieListItem.css';
 
+function selectGenre(e) {
+    console.log(e);
+}
+
+function selectYear(e) {
+    console.log(e);
+}
+
 export default function MovieListItem(props) {
+
+    const { movieStore } = useStore();
 
     const movie = props.data;
     const thumbnail = `/thumbs/${movie._id}.jpg`;
@@ -19,10 +30,18 @@ export default function MovieListItem(props) {
                     {movie.Title}
                 </div>
                 <div className="genre">
-                    {movie.Genre}
+                    {movie.Genre.map((genre, i) => {
+                        return (
+                            <div key={genre} onClick={() => selectGenre(genre)}>
+                                <span>{genre}</span>
+                                {i < movie.Genre.length - 1 ? 
+                                <i>|</i> : null}
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className="additional-details">
-                    <div className="year">
+                    <div className="year" onClick={() => selectYear(movie.Year)}>
                         {movie.Year}
                     </div>
                     <div className="runtime">
