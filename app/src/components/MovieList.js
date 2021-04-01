d9cd305bb2b30bb53452fded757f85c14d4f4d7d
Paul Berry <paulberry96@gmail.com>
2021-04-01 13:05:21 +1000

    Split view into left and right sections

--- app/src/components/MovieList.js
+++ app/src/components/MovieList.js
@@ -17,58 +17,64 @@ function MovieList() {
 
 	return (
 		<div className={`movie-list-wrapper ${uiStore.viewType}`}>
-			<div className="action-bar">
-				<div className="title">
-					<div>{movieStore.movies.length}</div><span>movies</span>
-				</div>
-				<div className="actions">
-					<div className="view-toggle">
-						<button onClick={uiStore.toggleView} className={`btn-toggle-view${uiStore.viewType === "grid-view" ? " selected" : ""}`}>
-							<FontAwesomeIcon icon={faTh} />
-						</button>
-						<button onClick={uiStore.toggleView} className={`btn-toggle-view${uiStore.viewType === "list-view" ? " selected" : ""}`}>
-							<FontAwesomeIcon icon={faList} />
-						</button>
-					</div>
-					<div className="filter">
-						<button onClick={uiStore.toggleFilters} className="btn-filter">
-							<FontAwesomeIcon icon={faFilter} />
-						</button>
-					</div>
-					<div className="sort">
-						<Dropdown options={sortOptions} onChange={movieStore.setSortOption} value={movieStore.sort.value} placeholder="Sort by" />
-						<button onClick={movieStore.toggleSortDir} className="btn-sort-dir">
-							<FontAwesomeIcon icon={movieStore.sort.dir === 1 ? faSortAmountDownAlt : faSortAmountUp} />
-						</button>
-					</div>
-				</div>
+			<div className="section-left">
+				<div className="filter-wrapper"></div>
 			</div>
-			<div className={`movie-list ${uiStore.viewType} v-scroll`}>
-
-				{(uiStore.viewType === "list-view") ?
-					<div className="list-header">
+			<div className="section-right">
+				<div className="action-bar">
+					<div className="title">
+						<div>{movieStore.movies.length}</div><span>movies</span>
+					</div>
+					<div className="actions">
+						<div className="view-toggle">
+							<button onClick={uiStore.toggleView} className={`btn-toggle-view${uiStore.viewType === "grid-view" ? " selected" : ""}`}>
+								<FontAwesomeIcon icon={faTh} />
+							</button>
+							<button onClick={uiStore.toggleView} className={`btn-toggle-view${uiStore.viewType === "list-view" ? " selected" : ""}`}>
+								<FontAwesomeIcon icon={faList} />
+							</button>
+						</div>
+						<div className="filter">
+							<button onClick={uiStore.toggleFilters} className="btn-filter">
+								<FontAwesomeIcon icon={faFilter} />
+							</button>
+						</div>
+						<div className="sort">
+							<Dropdown options={sortOptions} onChange={movieStore.setSortOption} value={movieStore.sort.value} placeholder="Sort" />
+							<button onClick={movieStore.toggleSortDir} className="btn-sort-dir">
+								<FontAwesomeIcon icon={movieStore.sort.dir === 1 ? faSortAmountDownAlt : faSortAmountUp} />
+							</button>
+						</div>
+					</div>
+				</div>
+				<div className={`movie-list-container v-scroll ${uiStore.viewType}`}>
+					<div className={`movie-list ${uiStore.viewType}`}>
+						{(uiStore.viewType === "list-view") ?
+							<div className="list-header">
+								{
+									movieStore.sort.options.map(opt => (
+										<div
+											key={opt.value}
+											onClick={() => { if(opt.enabled) movieStore.setSortOption(opt); }}
+											className={`${opt.enabled ? "sortable" : ""}`}>
+											<span>{opt.label}</span>
+											{opt.value === movieStore.sort.value ?
+												<FontAwesomeIcon className="sortArrow" icon={(movieStore.sort.dir === 0) ? faArrowUp : faArrowDown} />
+												: null
+											}
+										</div>
+									))
+								}
+							</div>
+							: null
+						}
 						{
-							movieStore.sort.options.map(opt => (
-								<div
-									key={opt.value}
-									onClick={() => { if(opt.enabled) movieStore.setSortOption(opt); }}
-									className={`${opt.enabled ? "sortable" : ""}`}>
-									<span>{opt.label}</span>
-									{opt.value === movieStore.sort.value ?
-										<FontAwesomeIcon className="sortArrow" icon={(movieStore.sort.dir === 0) ? faArrowUp : faArrowDown} />
-										: null
-									}
-								</div>
+							movieStore.movies.map(movieData => (
+								<MovieListItem key={movieData._id} data={movieData} viewType={uiStore.viewType} />
 							))
 						}
 					</div>
-					: null
-				}
-				{
-					movieStore.movies.map(movieData => (
-						<MovieListItem key={movieData._id} data={movieData} viewType={uiStore.viewType} />
-					))
-				}
+				</div>
 			</div>
 		</div>
 	);
