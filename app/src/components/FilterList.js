eac1939e9091ee09776a2ccae8d3017a020fb472
Paul Berry <paulberry96@gmail.com>
2021-04-10 14:38:51 +1000

    Refactor filter list

--- app/src/components/FilterList.js
+++ app/src/components/FilterList.js
@@ -28,9 +28,15 @@ function FilterList() {
 
     const { movieStore, uiStore } = useStore();
 
+    const filters = movieStore.filter.fields;
+    const currentFilters = movieStore.filter.current;
+    const uiFilters = uiStore.filterValues;
+
+    const loaded = movieStore.filter.loaded;
+
     return (
         <div className="filter-list-wrapper">
-            {(movieStore.filter.loaded) ?
+            {(loaded) ?
                 <div className="filter-list v-scroll">
                     <div className="title">
                         <span>Filters</span>
@@ -39,7 +45,7 @@ function FilterList() {
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Year</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Year")) ?
+                            {(currentFilters.hasOwnProperty("Year")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Year") }}>
                                     <span>Clear</span>
                                 </div>
@@ -49,23 +55,23 @@ function FilterList() {
                         <div className="filter-contents">
                             <div className="slider-wrapper">
                                 <Range allowCross={true}
-                                    min={movieStore.filter.fields.Year.min}
-                                    max={movieStore.filter.fields.Year.max}
-                                    value={uiStore.filterValues.Year}
-                                    defaultValue={[movieStore.filter.fields.Year.min, movieStore.filter.fields.Year.max]}
+                                    min={filters.Year.min}
+                                    max={filters.Year.max}
+                                    value={uiFilters.Year}
+                                    defaultValue={[filters.Year.min, filters.Year.max]}
                                     onChange={(val) => { uiStore.onFilterValueChange("Year", val) }}
                                     onAfterChange={(val) => { uiStore.onFilterValueChange("Year", val, true) }} />
                             </div>
                             <div className="slider-values">
-                                <span>{uiStore.filterValues.Year[0]}</span>
-                                <span>{uiStore.filterValues.Year[1]}</span>
+                                <span>{uiFilters.Year[0]}</span>
+                                <span>{uiFilters.Year[1]}</span>
                             </div>
                         </div>
                     </div>
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Genre</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Genre")) ?
+                            {(currentFilters.hasOwnProperty("Genre")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Genre") }}>
                                     <span>Clear</span>
                                 </div>
@@ -74,8 +80,8 @@ function FilterList() {
                         </div>
                         <div className="filter-contents">
                             <Dropdown
-                                options={movieStore.filter.fields.Genre.values}
-                                value={uiStore.filterValues.Genre}
+                                options={filters.Genre.values}
+                                value={uiFilters.Genre}
                                 onChange={(val) => { uiStore.onFilterValueChange('Genre', val, true) }}
                                 placeholder="Select Genre"
                                 menuClassName="v-scroll v-scroll-auto"
@@ -85,7 +91,7 @@ function FilterList() {
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Rating</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("imdbRating")) ?
+                            {(currentFilters.hasOwnProperty("imdbRating")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("imdbRating") }}>
                                     <span>Clear</span>
                                 </div>
@@ -95,24 +101,24 @@ function FilterList() {
                         <div className="filter-contents">
                             <div className="slider-wrapper">
                                 <Range allowCross={true}
-                                    min={movieStore.filter.fields.imdbRating.min}
-                                    max={movieStore.filter.fields.imdbRating.max}
+                                    min={filters.imdbRating.min}
+                                    max={filters.imdbRating.max}
                                     step={0.1}
-                                    value={uiStore.filterValues.imdbRating}
-                                    defaultValue={[movieStore.filter.fields.imdbRating.min, movieStore.filter.fields.imdbRating.max]}
+                                    value={uiFilters.imdbRating}
+                                    defaultValue={[filters.imdbRating.min, filters.imdbRating.max]}
                                     onChange={(val) => { uiStore.onFilterValueChange("imdbRating", val) }}
                                     onAfterChange={(val) => { uiStore.onFilterValueChange("imdbRating", val, true) }} />
                             </div>
                             <div className="slider-values">
-                                <span>{uiStore.filterValues.imdbRating[0]}</span>
-                                <span>{uiStore.filterValues.imdbRating[1]}</span>
+                                <span>{uiFilters.imdbRating[0]}</span>
+                                <span>{uiFilters.imdbRating[1]}</span>
                             </div>
                         </div>
                     </div>
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Runtime</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Runtime")) ?
+                            {(currentFilters.hasOwnProperty("Runtime")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Runtime") }}>
                                     <span>Clear</span>
                                 </div>
@@ -122,23 +128,23 @@ function FilterList() {
                         <div className="filter-contents">
                             <div className="slider-wrapper">
                                 <Range allowCross={true}
-                                    min={movieStore.filter.fields.Runtime.min}
-                                    max={movieStore.filter.fields.Runtime.max}
-                                    value={uiStore.filterValues.Runtime}
-                                    defaultValue={[movieStore.filter.fields.Runtime.min, movieStore.filter.fields.Runtime.max]}
+                                    min={filters.Runtime.min}
+                                    max={filters.Runtime.max}
+                                    value={uiFilters.Runtime}
+                                    defaultValue={[filters.Runtime.min, filters.Runtime.max]}
                                     onChange={(val) => { uiStore.onFilterValueChange("Runtime", val) }}
                                     onAfterChange={(val) => { uiStore.onFilterValueChange("Runtime", val, true) }} />
                             </div>
                             <div className="slider-values">
-                                <span>{uiStore.filterValues.Runtime[0]} mins</span>
-                                <span>{uiStore.filterValues.Runtime[1]} mins</span>
+                                <span>{uiFilters.Runtime[0]} mins</span>
+                                <span>{uiFilters.Runtime[1]} mins</span>
                             </div>
                         </div>
                     </div>
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Votes</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("imdbVotes")) ?
+                            {(currentFilters.hasOwnProperty("imdbVotes")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("imdbVotes") }}>
                                     <span>Clear</span>
                                 </div>
@@ -148,24 +154,24 @@ function FilterList() {
                         <div className="filter-contents">
                             <div className="slider-wrapper">
                                 <Range allowCross={true}
-                                    min={movieStore.filter.fields.imdbVotes.min}
-                                    max={movieStore.filter.fields.imdbVotes.max}
+                                    min={filters.imdbVotes.min}
+                                    max={filters.imdbVotes.max}
                                     step={1000}
-                                    value={uiStore.filterValues.imdbVotes}
-                                    defaultValue={[movieStore.filter.fields.imdbVotes.min, movieStore.filter.fields.imdbVotes.max]}
+                                    value={uiFilters.imdbVotes}
+                                    defaultValue={[filters.imdbVotes.min, filters.imdbVotes.max]}
                                     onChange={(val) => { uiStore.onFilterValueChange("imdbVotes", val) }}
                                     onAfterChange={(val) => { uiStore.onFilterValueChange("imdbVotes", val, true) }} />
                             </div>
                             <div className="slider-values">
-                                <span>{nFormatter(uiStore.filterValues.imdbVotes[0], 1)}</span>
-                                <span>{nFormatter(uiStore.filterValues.imdbVotes[1], 1)}</span>
+                                <span>{nFormatter(uiFilters.imdbVotes[0], 1)}</span>
+                                <span>{nFormatter(uiFilters.imdbVotes[1], 1)}</span>
                             </div>
                         </div>
                     </div>
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Rated</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Rated")) ?
+                            {(currentFilters.hasOwnProperty("Rated")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Rated") }}>
                                     <span>Clear</span>
                                 </div>
@@ -174,8 +180,8 @@ function FilterList() {
                         </div>
                         <div className="filter-contents">
                             <Dropdown
-                                options={movieStore.filter.fields.Rated.values}
-                                value={uiStore.filterValues.Rated}
+                                options={filters.Rated.values}
+                                value={uiFilters.Rated}
                                 onChange={(val) => { uiStore.onFilterValueChange('Rated', val, true) }}
                                 placeholder="Select Rating"
                                 menuClassName="v-scroll v-scroll-auto" />
@@ -184,7 +190,7 @@ function FilterList() {
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Country</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Country")) ?
+                            {(currentFilters.hasOwnProperty("Country")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Country") }}>
                                     <span>Clear</span>
                                 </div>
@@ -193,8 +199,8 @@ function FilterList() {
                         </div>
                         <div className="filter-contents">
                             <Dropdown
-                                options={movieStore.filter.fields.Country.values}
-                                value={uiStore.filterValues.Country}
+                                options={filters.Country.values}
+                                value={uiFilters.Country}
                                 onChange={(val) => { uiStore.onFilterValueChange('Country', val, true) }}
                                 placeholder="Select Country"
                                 menuClassName="v-scroll v-scroll-auto"
@@ -204,7 +210,7 @@ function FilterList() {
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Language</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("Language")) ?
+                            {(currentFilters.hasOwnProperty("Language")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("Language") }}>
                                     <span>Clear</span>
                                 </div>
@@ -213,8 +219,8 @@ function FilterList() {
                         </div>
                         <div className="filter-contents">
                             <Dropdown
-                                options={movieStore.filter.fields.Language.values}
-                                value={uiStore.filterValues.Language}
+                                options={filters.Language.values}
+                                value={uiFilters.Language}
                                 onChange={(val) => { uiStore.onFilterValueChange('Language', val, true) }}
                                 placeholder="Select Language"
                                 menuClassName="v-scroll v-scroll-auto"
@@ -224,7 +230,7 @@ function FilterList() {
                     <div className="filter-section">
                         <div className="filter-header">
                             <div className="filter-title"><span>Box Office</span></div>
-                            {(movieStore.filter.current.hasOwnProperty("BoxOffice")) ?
+                            {(currentFilters.hasOwnProperty("BoxOffice")) ?
                                 <div className="filter-reset" onClick={() => { uiStore.clearFilter("BoxOffice") }}>
                                     <span>Clear</span>
                                 </div>
@@ -234,17 +240,17 @@ function FilterList() {
                         <div className="filter-contents">
                             <div className="slider-wrapper">
                                 <Range allowCross={true}
-                                    min={movieStore.filter.fields.BoxOffice.min}
-                                    max={movieStore.filter.fields.BoxOffice.max}
+                                    min={filters.BoxOffice.min}
+                                    max={filters.BoxOffice.max}
                                     step={1000000}
-                                    value={uiStore.filterValues.BoxOffice}
-                                    defaultValue={[movieStore.filter.fields.BoxOffice.min, movieStore.filter.fields.BoxOffice.max]}
+                                    value={uiFilters.BoxOffice}
+                                    defaultValue={[filters.BoxOffice.min, filters.BoxOffice.max]}
                                     onChange={(val) => { uiStore.onFilterValueChange("BoxOffice", val) }}
                                     onAfterChange={(val) => { uiStore.onFilterValueChange("BoxOffice", val, true) }} />
                             </div>
                             <div className="slider-values">
-                                <span>{"$" + nFormatter(uiStore.filterValues.BoxOffice[0], 1)}</span>
-                                <span>{"$" + nFormatter(uiStore.filterValues.BoxOffice[1], 1)}</span>
+                                <span>{"$" + nFormatter(uiFilters.BoxOffice[0], 1)}</span>
+                                <span>{"$" + nFormatter(uiFilters.BoxOffice[1], 1)}</span>
                             </div>
                         </div>
                     </div>
